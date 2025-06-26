7cd397097f8147fa9e29993ae0319ac07de9beb7
Andrés Cruz <andres.cruz@utexas.edu>
2025-06-26 14:00:11 -0500

    Add tolerance to tests

--- inst/tinytest/test_predictions.R
+++ inst/tinytest/test_predictions.R
@@ -1,3 +1,8 @@
+# occasionally autodiff is more precise than numerical differentiation.
+# must allow some tolerance
+tol <- 1e-5
+
+
 library(marginaleffects)
 mod <- lm(mpg ~ hp, mtcars)
 
@@ -8,8 +13,8 @@ preds_raw <- predictions(mod)
 enable_JAX_backend()
 preds_jax <- predictions(mod)
 
-expect_equal(preds_jax$estimate, preds_raw$estimate)
-expect_equal(preds_jax$std.error, preds_raw$std.error)
+expect_equal(preds_jax$estimate, preds_raw$estimate, tolerance = tol)
+expect_equal(preds_jax$std.error, preds_raw$std.error, tolerance = tol)
 
 # predictions(, by = T) ----
 
@@ -18,8 +23,8 @@ preds_byT_jax <- predictions(mod, by = T)
 disable_JAX_backend()
 preds_byT_raw <- predictions(mod, by = T)
 
-expect_equal(preds_byT_jax$estimate, preds_byT_raw$estimate)
 expect_equal(preds_byT_jax$std.error, preds_byT_raw$std.error)
+expect_equal(preds_byT_jax$std.error, preds_byT_raw$std.error, tolerance = tol)
 
 # avg_predictions() ----
 
@@ -28,5 +33,5 @@ avg_preds_raw <- avg_predictions(mod)
 enable_JAX_backend()
 avg_preds_jax <- avg_predictions(mod)
 
-expect_equal(avg_preds_jax$estimate, avg_preds_raw$estimate)
-expect_equal(avg_preds_jax$std.error, avg_preds_raw$std.error)
+expect_equal(avg_preds_jax$estimate, avg_preds_raw$estimate, tolerance = tol)
+expect_equal(avg_preds_jax$std.error, avg_preds_raw$std.error, tolerance = tol)

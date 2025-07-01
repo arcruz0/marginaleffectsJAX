208d95207c703be24c0a4595aac6db673d3d8344
Andrés Cruz <andres.cruz@utexas.edu>
2025-07-01 13:59:05 -0500

    Combine tests for `predictions()` and `plot_predictions()`

--- inst/tinytest/test_predictions.R
+++ inst/tinytest/test_predictions.R
@@ -1,4 +1,8 @@
-test_predictions <- function(expr_preds, tolerance = 1e-5){
+# occasionally autodiff is more precise than numerical differentiation.
+# must allow some tolerance
+tol <- 1e-5
+
+test_predictions <- function(expr_preds, tolerance = tol){
   enable_JAX_backend(verbose = T)
   expect_message(eval(expr_preds), "Succesfully executed JAX function")
   preds_jax <- eval(expr_preds)
@@ -7,6 +11,18 @@ test_predictions <- function(expr_preds, tolerance = 1e-5){
   
   expect_equal(preds_jax$estimate, preds_no_jax$estimate, tolerance = tolerance)
   expect_equal(preds_jax$std.error, preds_no_jax$std.error, tolerance = tolerance)
+}
+
+test_plot_predictions <- function(expr_plot_preds, tolerance = tol){
+  enable_JAX_backend(verbose = T)
+  expect_message(eval(expr_plot_preds), "Succesfully executed JAX function")
+  plot_preds_jax <- eval(expr_plot_preds)
+  disable_JAX_backend()
+  plot_preds_no_jax <- eval(expr_plot_preds)
+  
+  expect_equal(ggplot2::ggplot_build(plot_preds_jax)$data, 
+               ggplot2::ggplot_build(plot_preds_no_jax)$data, 
+               tolerance = tolerance)
 }
 
 library(marginaleffects)
@@ -50,6 +66,10 @@ test_predictions(
   predictions(mod, by = "dummy_female")
 )
 
+test_plot_predictions(
+  plot_predictions(mod, by = "dummy_female")
+)
+
 ## character
 
 mod_chr <- lm(bill_len ~ bill_dep + species_chr,
@@ -59,12 +79,20 @@ test_predictions(
   predictions(mod_chr, by = "species_chr")
 )
 
+test_plot_predictions(
+  plot_predictions(mod_chr, by = "species_chr")
+)
+
 ## factor: pre-existing factor variable
 
 mod_factor_pre <- lm(bill_len ~ bill_dep + species, penguins2)
 
 test_predictions(
   predictions(mod_factor_pre, by = "species")
+)
+
+test_plot_predictions(
+  plot_predictions(mod_factor_pre, by = "species")
 )
 
 ## factor: character variable passed as factor()
@@ -78,6 +106,10 @@ test_predictions(
   predictions(mod_factor_from_chr, by = "species_chr")
 )
 
+test_plot_predictions(
+  plot_predictions(mod_factor_from_chr, by = "species_chr")
+)
+
 # factor: integer variable passed as factor()
 mod_factor_from_int <-  lm(bill_len ~ bill_dep + factor(year), penguins2)
 
@@ -85,11 +117,22 @@ test_predictions(
   predictions(mod_factor_from_int, by = "year")
 )
 
+test_plot_predictions(
+  plot_predictions(mod_factor_from_int, by = "year")
+)
+
 # factor: numeric variable passed as factor()
-mod_factor_from_num <- lm(bill_len ~ bill_dep + factor(bill_dep), penguins2)
+mod_factor_from_num <- lm(
+  bill_len ~ bill_dep + factor(year_dbl), 
+  penguins2 |> transform(year_dbl = as.numeric(year))
+)
 
 test_predictions(
   predictions(mod_factor_from_num, by = "bill_dep")
+)
+
+test_plot_predictions(
+  plot_predictions(mod_factor_from_num, by = "bill_dep")
 )
 
 # factor: variable passed as as.factor()
@@ -100,5 +143,9 @@ mod_factor_from_chr_as.factor <- lm(
 
 test_predictions(
   predictions(mod_factor_from_chr_as.factor, by = "species_chr")
+)
+
+test_plot_predictions(
+  plot_predictions(mod_factor_from_chr_as.factor, by = "species_chr")
 )
 
